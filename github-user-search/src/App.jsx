d69c26e3249497863e0810ceccb2c76c965ac81c
Desimeon <sbdaramola@gmail.com>
2025-11-25 12:12:04 +0100

    adding new files

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,13 +1,51 @@
 import { apiKey } from "./services/githubApi"
+import { useState } from "react";
+import Search from "./components/Search";
+import { fetchUserData } from "./services/githubService";
 
 function App() {
- 
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleSearch = async (username) => {
+    setLoading(true);
+    setError("");
+    setUser(null);
+
+    try {
+      const data = await fetchUserData(username);
+      setUser(data);
+    } catch (err) {
+      setError("Looks like we can't find the user.");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
-    <>
-    <p></p>
-    </>
-  )
+    <div className="app">
+      <h1>GitHub User Search</h1>
+      <Search onSearch={handleSearch} />
+
+      
+      {loading && <p>Loading...</p>}
+
+    
+      {error && <p>{error}</p>}
+
+     
+      {user && (
+        <div className="user-card">
+          <img src={user.avatar_url} alt={user.login} width="120" />
+          <h2>{user.name || user.login}</h2>
+          <a href={user.html_url} target="_blank" rel="noreferrer">
+            View Profile
+          </a>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
